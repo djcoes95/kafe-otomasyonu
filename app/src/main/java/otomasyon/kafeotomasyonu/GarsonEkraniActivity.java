84bedbc98cafa4ca750ac79df895b15a984958a8
Bilal <bilal@teknovol.com.tr>
2017-03-25 12:40:25 +0300

    GarsonActivity geliştirildi

--- app/src/main/java/otomasyon/kafeotomasyonu/GarsonEkraniActivity.java
+++ app/src/main/java/otomasyon/kafeotomasyonu/GarsonEkraniActivity.java
@@ -41,7 +41,6 @@ public class GarsonEkraniActivity extends AppCompatActivity {
                 butonOlustur(Integer.parseInt(dataSnapshot.getChildrenCount()+""));
 
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // Getting Post failed, log a message
@@ -51,57 +50,31 @@ public class GarsonEkraniActivity extends AppCompatActivity {
         });
         return masaSayisi;
     }
-
     void butonOlustur(int gelen){
-        int satir=1,sutun=4,butonsira=0,butonsira2=1;
-        Toast.makeText(this, String.valueOf(gelen), Toast.LENGTH_SHORT).show();
+        int satir=1,sutun=4,masaid=1;
         GridLayout gridLayout = (GridLayout)findViewById(R.id.activity_garson);
         ///HATA VAR DÜZELT MASA SATIR SÜTÜN BOZUK GELİYOR
         if(gelen>5){
-            satir = (gelen%4)+1;
-
+            satir = (gelen/4)+1;
         }
         gridLayout.setColumnCount(sutun);
         gridLayout.setRowCount(satir);
-        for(int r = 1; r <satir; r++ ){
+        for(int i=0;i<gelen;i++){
+            Button myButton = new Button(this);
+                myButton.setId(masaid);
+                myButton.setText("Masa " + masaid);
+                gridLayout.addView(myButton,i);
+            masaid++;
+            final int id=myButton.getId();
+            myButton = ((Button) findViewById(id));
 
-            for(int c = 1; c < sutun; c++ ){
-                Button myButton = new Button(this);
-                myButton.setId(butonsira);
-                myButton.setText("Masa " + butonsira2);
-                gridLayout.addView(myButton,butonsira);
-                butonsira++;
-                butonsira2++;
+            myButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {  // butonun click olayi
+                Toast.makeText(view.getContext(),
+                        "Masa "+ id +"'ya tiklandi", Toast.LENGTH_SHORT)
+                        .show();
             }
+        });
         }
-
     }
-
-
-//    void butonOlustur(int gelen){
-//        int x=150,y=300;
-//        for(int i = 1;i<=gelen;i++){
-//
-//        Button myButton = new Button(this);
-//        final RelativeLayout ll = (RelativeLayout)findViewById(R.id.activity_garson);  // butonun eklenecegi layout tanimlandi
-//        myButton.setId(i);   // butona id atandi
-//        myButton.setX(x);  // butona x koordinati verildi
-//        myButton.setY(y);  // butona y koordinati verildi
-//        myButton.setText(" "+i);
-//        final int id=myButton.getId();
-//
-//        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(200,100);  // butonun boyutlari verildi
-//        ll.addView(myButton, lp);
-//        myButton = ((Button) findViewById(id));
-//        myButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View view) {  // butonun click olayi
-//                Toast.makeText(view.getContext(),
-//                        "Button "+ id +" tiklandi", Toast.LENGTH_SHORT)
-//                        .show();
-//            }
-//        });
-//            x+=100;
-//            y+=100;
-//        }
-//    }
 }
