2bf195a176759a4d61baded40f1da204fbf36d84
Bilal <bilal@teknovol.com.tr>
2017-03-26 17:22:06 +0300

    Son Hali

--- app/src/main/java/otomasyon/kafeotomasyonu/SiparisEkle.java
+++ app/src/main/java/otomasyon/kafeotomasyonu/SiparisEkle.java
@@ -22,14 +22,33 @@ public class SiparisEkle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siparis_ekle);
-        siparisGoster();
+        //siparisGoster();
         
     }
 
-    private void siparisGoster() {
-
-        urunYazdir();
-    }
+//    private void siparisGoster() {
+//        DatabaseReference myRef = database.getReference("urunler");
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                dataSnapshot  = dataSnapshot.child("1");
+//                String fiyat = (String) dataSnapshot.getValue();
+//                //Urunler u = dataSnapshot.getValue(Urunler.class);
+//                urunlerr.add(new UrunlerListe(u.getUrunadi(),fiyat));
+////                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+////                    Urunler u = postSnapshot.child(1+"").getValue(Urunler.class);
+////                    urunlerr.add(new UrunlerListe(u.getUrunadi(),u.getUrunfiyati()));
+////                }
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                // Getting Post failed, log a message
+//                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+//                // ...
+//            }
+//        });
+//        urunYazdir();
+//    }
 
     private void urunYazdir() {
         System.out.println(urunlerr);
