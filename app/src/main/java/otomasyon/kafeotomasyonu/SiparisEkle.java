c94a0e62b855af6ab4a804014e379832acc8b33a
dilolokko <mahmutyetisir03@gmail.com>
2017-03-26 09:38:21 -0700

    SiparisEkle AlertDialog Eklendi Veriler internete yüklenmesi kaldı

--- app/src/main/java/otomasyon/kafeotomasyonu/SiparisEkle.java
+++ app/src/main/java/otomasyon/kafeotomasyonu/SiparisEkle.java
@@ -1,7 +1,16 @@
 package otomasyon.kafeotomasyonu;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,35 +31,108 @@ public class SiparisEkle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siparis_ekle);
-        //siparisGoster();
+        urunSayisi();
+        siparisGoster();
         
     }
 
-//    private void siparisGoster() {
-//        DatabaseReference myRef = database.getReference("urunler");
-//        myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                dataSnapshot  = dataSnapshot.child("1");
-//                String fiyat = (String) dataSnapshot.getValue();
-//                //Urunler u = dataSnapshot.getValue(Urunler.class);
-//                urunlerr.add(new UrunlerListe(u.getUrunadi(),fiyat));
-////                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-////                    Urunler u = postSnapshot.child(1+"").getValue(Urunler.class);
-////                    urunlerr.add(new UrunlerListe(u.getUrunadi(),u.getUrunfiyati()));
-////                }
-//            }
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                // Getting Post failed, log a message
-//                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-//                // ...
-//            }
-//        });
-//        urunYazdir();
-//    }
+    private void siparisGoster() {
+        DatabaseReference myRef = database.getReference("urunler");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (int i=1;i<=Integer.parseInt(dataSnapshot.getChildrenCount()+"");i++)
+                {
+                    String urunadi = (String) dataSnapshot.child(String.valueOf(i)).child("urunadi").getValue();
+                    String fiyat = (String) String.valueOf(dataSnapshot.child(String.valueOf(i)).child("urunfiyati").getValue());
+                    urunlerr.add(new UrunlerListe(urunadi,fiyat));
+                    butonOlustur(i,urunadi);
+                    Toast.makeText(SiparisEkle.this, urunadi, Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        });
+    }
+    private void urunSayisi() {
+        String urunSayisi="0";
+        DatabaseReference myRef = database.getReference("urunler");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                urunButonOlustur(Integer.parseInt(dataSnapshot.getChildrenCount()+""));
 
-    private void urunYazdir() {
-        System.out.println(urunlerr);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        });
+    }
+    void butonOlustur(int urunid,String urunAdi)
+    {
+            GridLayout gridLayout = (GridLayout)findViewById(R.id.urunler);
+            Button myButton = new Button(this);
+            myButton.setId(urunid);
+            myButton.setText(urunAdi);
+            //myButton.setTextAppearance(this,R.style.butonlar);
+            urunid--;
+            gridLayout.addView(myButton,urunid);
+            final int id=myButton.getId();
+            myButton = ((Button) findViewById(id));
+            myButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {  // butonun click olayi
+                    Toast.makeText(view.getContext(),
+                            "Urun "+ id +"'ya tiklandi", Toast.LENGTH_SHORT)
+                            .show();
+                    Button inputButton = (Button) findViewById(id);
+                    inputDialog(inputButton.getText().toString());
+                    //Intent i = new Intent(SiparisEkle.this, SiparisEkle.class);
+                    //startActivity(i);
+
+                }
+            });
+    }
+    void urunButonOlustur(int gelen){
+        int satir=1,sutun=4;
+        GridLayout gridLayout = (GridLayout)findViewById(R.id.urunler);
+        if(gelen>5){
+            satir = (gelen/4)+1;
+        }
+        gridLayout.setColumnCount(sutun);
+        gridLayout.setRowCount(satir);
+    }
+    void inputDialog(String urunAdi)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(urunAdi);
+
+// Set up the input
+        final EditText input = new EditText(this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+// Set up the buttons
+        builder.setPositiveButton("Tamam", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String m_Text = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("İptal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
